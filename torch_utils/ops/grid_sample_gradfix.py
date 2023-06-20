62d070b385b173d3d693ddc2c19e1aefe3835c2b
Tanut Apiwong <tanutapi@gmail.com>
2023-06-20 11:14:08 +0700

    https://github.com/NVlabs/stylegan3/commit/407db86e6fe432540a22515310188288687858fa

--- torch_utils/ops/grid_sample_gradfix.py
+++ torch_utils/ops/grid_sample_gradfix.py
@@ -22,6 +22,7 @@ from distutils.version import LooseVersion
 #----------------------------------------------------------------------------
 
 enabled = False  # Enable the custom op by setting this to true.
+_use_pytorch_1_11_api = LooseVersion(torch.__version__) >= LooseVersion('1.11.0a')
 
 #----------------------------------------------------------------------------
 
@@ -62,8 +63,14 @@ class _GridSample2dForward(torch.autograd.Function):
 class _GridSample2dBackward(torch.autograd.Function):
     @staticmethod
     def forward(ctx, grad_output, input, grid):
-        op, _ = torch._C._jit_get_operation('aten::grid_sampler_3d_backward')
-        grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False)
+        op, _ = torch._C._jit_get_operation('aten::grid_sampler_2d_backward')
+
+        if _use_pytorch_1_11_api:
+            output_mask = (ctx.needs_input_grad[1], ctx.needs_input_grad[2])
+            grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False, output_mask)
+        else:
+            grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False)
+
         ctx.save_for_backward(grid)
         return grad_input, grad_grid
 
